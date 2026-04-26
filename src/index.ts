ac581c1ac20a35b98c5c0d56467ed7618ef07aec
Cristian Florez <ingenieroflorezco@gmail.com>
2026-04-25 22:46:31 -0500

    feat: export all new components

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export { Badge, badgeVariants, type BadgeProps } from "./components/Badge/Badge"
 export { Modal, type ModalProps } from "./components/Modal/Modal";
 export { EmptyState, type EmptyStateProps } from "./components/EmptyState/EmptyState";
 export { Spinner, type SpinnerProps } from "./components/Spinner/Spinner";
-export { Tabs, TabsList, TabsTrigger, TabsContent, type TabsProps, type TabsListProps, type TabsTriggerProps, type TabsContentProps } from "./components/Tabs/Tabs";
+export { Tabs, TabsList, TabsTrigger, TabsContent } from "./components/Tabs/Tabs";
 export { Table, TableHeader, TableBody, TableRow, TableHead, TableCell } from "./components/Table/Table";
 export { Divider, type DividerProps } from "./components/Divider/Divider";
 export { Text, textVariants, type TextProps } from "./components/Text/Text";
@@ -16,6 +16,25 @@ export { Switch, type SwitchProps } from "./components/Switch/Switch";
 export { Label, type LabelProps } from "./components/Label/Label";
 export { Select, type SelectProps } from "./components/Select/Select";
 export { FormMessage, type FormMessageProps } from "./components/FormMessage/FormMessage";
+export { Avatar, type AvatarProps } from "./components/Avatar/Avatar";
+export { Tooltip, type TooltipProps } from "./components/Tooltip/Tooltip";
+export { Skeleton, type SkeletonProps } from "./components/Skeleton/Skeleton";
+export { Progress, type ProgressProps } from "./components/Progress/Progress";
+export { Kbd } from "./components/Kbd/Kbd";
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "./components/Accordion/Accordion";
+export { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "./components/Breadcrumb/Breadcrumb";
+export { Drawer, type DrawerProps } from "./components/Drawer/Drawer";
+export { Checkbox, type CheckboxProps } from "./components/Checkbox/Checkbox";
+export { RadioGroup, RadioItem } from "./components/Radio/Radio";
+export { IconButton, type IconButtonProps } from "./components/IconButton/IconButton";
+export { Heading, type HeadingProps } from "./components/Heading/Heading";
+export { Code } from "./components/Code/Code";
+export { Tag, type TagProps } from "./components/Tag/Tag";
+export { Status, type StatusProps } from "./components/Status/Status";
+export { Popover, type PopoverProps } from "./components/Popover/Popover";
+export { Pagination, type PaginationProps } from "./components/Pagination/Pagination";
+export { Stack, type StackProps } from "./components/Stack/Stack";
+export { VisuallyHidden } from "./components/VisuallyHidden/VisuallyHidden";
 
 // Layouts
 export { Container, type ContainerProps } from "./layouts/Container/Container";
